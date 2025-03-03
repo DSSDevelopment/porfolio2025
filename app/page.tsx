7b94c2948396cfa6d4722364f1c450d915631836
DSSDevelopment <dead4aday@icloud.com>
2025-03-03 14:52:04 -0600

    Working timeline components with header bubble and adjustable positioning.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,44 @@
 import "./timeline.css";
+import TimelineBubble from "./timelineBubble";
 
 export default function Home() {
   return (
     <div>
-      <div className="timeline-header"></div>
+      <div className="timeline-header">
+        <div
+          style={{
+            display: "flex",
+            alignItems: "flex-end",
+            justifyContent: "center",
+            height: "100%",
+          }}
+        >
+          <div className="timeline-bubble hairline-border">
+            <p style={{ fontWeight: 600 }}>About Me</p>
+            <p>
+              Lorem ipsum and all that kind of stuff that tests these things
+            </p>
+            <p>+510 260-2309</p>
+          </div>
+        </div>
+      </div>
       <div className="timeline-container">
         <div className="timeline"></div>
+        <TimelineBubble
+          title="Test Bubble"
+          text={["Highlight 1", "Highlight 2"]}
+          altitude={20}
+          progressPercent={0}
+        />
+        <TimelineBubble
+          title="Test Bubble 2"
+          text={[
+            "Highlight X",
+            "Much, much longer Highlight Y that might very well break the layout of this control and should be adjusted for!",
+          ]}
+          altitude={-10}
+          progressPercent={0}
+        />
       </div>
       <div className="timeline-footer"></div>
     </div>
--- app/timelineBubble.tsx
+++ app/timelineBubble.tsx
@@ -1,3 +1,5 @@
+import { CSSProperties } from "react";
+
 type BubbleProps = {
   title?: string;
   text: string[];
@@ -5,13 +7,33 @@ type BubbleProps = {
   progressPercent: number;
 };
 
-let props: BubbleProps = {
-  title: "Title",
-  text: ["Highlihgt 1", "Highlight 2"],
-  altitude: 20,
-  progressPercent: 0,
-};
-
 export default function TimelineBubble(props: BubbleProps) {
-  return <div></div>;
+  let bubbleFlexDirection = (props.altitude ?? 0) >= 0 ? "row" : "row-reverse";
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: bubbleFlexDirection === "row" ? "row" : "row-reverse",
+        alignItems: "center",
+        width: "50vw",
+        paddingTop: "20px",
+        transform: `translateX(${bubbleFlexDirection === "row" ? "50vw" : "0vw"})`,
+      }}
+    >
+      <div
+        style={{
+          width: `${Math.abs(props.altitude ?? 0)}vw`,
+          height: "4px",
+          backgroundColor: "#000000",
+        }}
+      ></div>
+      <div className="timeline-bubble hairline-border">
+        {props.title ? <p style={{ fontWeight: 600 }}>{props.title}</p> : []}
+        {props.text.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
